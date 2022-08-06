f9f43a36200adb563c577e6cd2df67ff6a6fde26
Santiago <santiagov801@gmail.com>
2022-08-06 01:14:03 -0500

    gsap temp removal

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -11,31 +11,31 @@ export default function Logo() {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin)
+  //   gsap
+  //     .timeline()
+  //     .to(bgRef.current, {
+  //       duration: 1,
+  //       opacity: 1,
+  //     })
+  //     .from(outlineLogoRef.current, {
+  //       drawSVG: 0,
+  //       duration: 20,
+  //     })
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 2,
-        duration: 4,
-      }
-    )
-  }, [])
+  //   gsap.fromTo(
+  //     solidLogoRef.current,
+  //     {
+  //       opacity: 0,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       delay: 2,
+  //       duration: 4,
+  //     }
+  //   )
+  // }, [])
   return (
     <div className="logo-container" ref={bgRef}>
       <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt="S" />
