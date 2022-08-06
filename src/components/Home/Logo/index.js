333c71858970cc99a1c8df436faa2252a690636c
Santiago <santiagov801@gmail.com>
2022-08-06 01:23:04 -0500

    gsap bug fix

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -2,8 +2,6 @@ import "./index.scss"
 import LogoS from "../../../assets/images/logo-s.png"
 import LogoLines from "../../../assets/images/logo-lines.svg"
 import React, { useRef, useEffect } from "react"
-// import gsap from "gsap-trial"
-// import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin"
 import GlitchSquiggly from "react-glitch-effect/core/GlitchSquiggly"
 
 export default function Logo() {
@@ -11,31 +9,6 @@ export default function Logo() {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  // useEffect(() => {
-  //   gsap.registerPlugin(DrawSVGPlugin)
-  //   gsap
-  //     .timeline()
-  //     .to(bgRef.current, {
-  //       duration: 1,
-  //       opacity: 1,
-  //     })
-  //     .from(outlineLogoRef.current, {
-  //       drawSVG: 0,
-  //       duration: 20,
-  //     })
-
-  //   gsap.fromTo(
-  //     solidLogoRef.current,
-  //     {
-  //       opacity: 0,
-  //     },
-  //     {
-  //       opacity: 1,
-  //       delay: 2,
-  //       duration: 4,
-  //     }
-  //   )
-  // }, [])
   return (
     <div className="logo-container" ref={bgRef}>
       <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt="S" />
